06f190efba6c2930780b72d76427e141d74ea29f
Thaddeus Ryker <thad@edgerunner.org>
2026-03-11 23:48:24 -0400

    feat: add Colors utility with quest state color constants

---
+++ Util/Colors.lua
@@ -0,0 +1,16 @@
+-- Util/Colors.lua
+-- Color constants used across SocialQuest UI files.
+
+SocialQuestColors = {
+    -- Quest state colors
+    active    = "|cFFFFFF00",  -- yellow
+    completed = "|cFF00FF00",  -- green
+    failed    = "|cFFFF0000",  -- red
+    unknown   = "|cFF888888",  -- grey
+    header    = "|cFFFFD700",  -- gold
+    chain     = "|cFF00CCFF",  -- cyan
+    timer     = "|cFFFF8C00",  -- orange
+    -- General
+    white     = "|cFFFFFFFF",
+    reset     = "|r",
+}
